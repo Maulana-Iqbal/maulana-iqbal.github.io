a01c6d38567b2f5c67973f817ab5eeda04199fde
Maulana-Iqbal <68462265+Maulana-Iqbal@users.noreply.github.com>
2024-06-07 16:46:25 +0700

    Update script.js

--- dev/udangan-nikah-iqbal-fauziah/script.js
+++ dev/udangan-nikah-iqbal-fauziah/script.js
@@ -283,16 +283,16 @@ openButton.addEventListener('click', function () {
 	audioPlayerDiv.style.right = '20px'; // Anda dapat menyesuaikan posisi sesuai kebutuhan
 });
 
-// Mencegah Klik Kanan
-document.addEventListener('contextmenu', function (event) {
-	event.preventDefault();
-});
+// // Mencegah Klik Kanan
+// document.addEventListener('contextmenu', function (event) {
+// 	event.preventDefault();
+// });
 
-// Mendefinisikan fungsi untuk menangani event keyboard
-function handleKeyDown(event) {
-	// Mencegah tindakan default yang dilakukan oleh tombol keyboard
-	event.preventDefault();
-}
+// // Mendefinisikan fungsi untuk menangani event keyboard
+// function handleKeyDown(event) {
+// 	// Mencegah tindakan default yang dilakukan oleh tombol keyboard
+// 	event.preventDefault();
+// }
 
-// Menambahkan event listener ke document untuk menangani event keydown
-document.addEventListener('keydown', handleKeyDown);
+// // Menambahkan event listener ke document untuk menangani event keydown
+// document.addEventListener('keydown', handleKeyDown);
